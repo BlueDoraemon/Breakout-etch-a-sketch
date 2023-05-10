e4be5926feee665843ded05c78b568b35e2c19d9
BlueDoraemon <3803480+BlueDoraemon@users.noreply.github.com>
2023-05-10 23:58:36 +1000

    Fixed ball collision feature That took me a while

--- script.js
+++ script.js
@@ -10,10 +10,15 @@ function makeGrid(gridLength){
     for (let i = 0; i < gridLength; i++){
         const row = document.createElement(`div`);
 
-        for (let i = 0; i < gridLength; i++) {
+        for (let j = 0; j < gridLength; j++) {
   
             const box = document.createElement('div');
             box.classList.add(`box`);
+            box.setAttribute('id',`i${i}j${j}`); // set x and y as i and j
+
+            box.addEventListener("mouseover", (e) => {
+                box.classList.add('red'); 
+            });
             row.appendChild(box);
         }
     grid.appendChild(row);
@@ -22,7 +27,45 @@ function makeGrid(gridLength){
 
 makeGrid(gridLength);
 
+
+const ball = document.querySelector('.ball');
+let x = 7;
+let y = 0;
+let dx = 1;
+let dy = 1;
+let intervalId = setInterval(moveBall, 50);
+
+function moveBall() {
+    // check if the ball hits the walls
+    if (x + dx < 0 || x + dx > 16) {
+        dx = -dx;
+    }
+    if (y + dy < 0 || y + dy > 18.5) {
+        dy = -dy;
+    }
+
+    // get the current grid item
+
+    let nthX = (x).toFixed(0);
+    let nthY = (gridLength-y).toFixed(0);
+    const currentItem = document.querySelector(`#i${nthX}j${nthY}`);
+    
+    if (currentItem) {
+        if (currentItem.classList.contains(`red`)) {
+          dy = -dy;
+          currentItem.classList.remove(`red`);
+        }
+    }
+    else console.log(currentItem);
+    // update the ball's position and velocity
+			x += dx;
+			y += dy;
+			ball.style.left = `${x * 50}px`;
+			ball.style.bottom = `${y * 50}px`;
+}
 //function to get input from user to create a grid
+
+
 
 //function to get key press from 
 
